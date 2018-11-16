4ad73230dae81146c817f145927adc0444106f54
PAN Zhiqi <zhiqipan@yahoo.com>
2018-11-16 12:38:16 +0800

    deployable

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 3000;
+const app = express();
+
+// the __dirname is the current directory from where the script is running
+const publicPath = path.join(__dirname, 'dist')
+app.use('/static', express.static(publicPath));
+
+// send the user to index html page inspite of the url
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(path.join(__dirname, 'dist'), 'app.html'));
+});
+
+app.listen(port);
